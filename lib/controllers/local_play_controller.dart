c5c43ccda7b44b6316e5b2da3111233a885459b2
jordanadamstn <jordan.adams@tn.gov>
2023-03-17 15:05:15 -0500

    working add player logic and validation

--- lib/controllers/local_play_controller.dart
+++ lib/controllers/local_play_controller.dart
@@ -1,10 +1,110 @@
-// local_play_controller.dart
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../constants/app_colors.dart';
+import 'dart:async';
 
 class LocalPlayController extends GetxController {
-  final RxList<String> usernames = RxList<String>(); // Use RxList<String>
+  final usernames = <String>[].obs;
+
+  final showInputField = false.obs;
+
+  Timer? _usernameValidationTimer;
+
+  var usernameInputBorder = OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.grey),
+  ).obs;
+
+  final usernameFocusNode = FocusNode();
+
+  var errorMessage = ''.obs;
+
+  final TextEditingController usernameController = TextEditingController();
+
+  final canProceed = false.obs; // Add the new observable variable
+
+  @override
+  void onInit() {
+    super.onInit();
+    usernameController.addListener(validateForm);
+  }
+
+  @override
+  void onClose() {
+    usernameController.removeListener(validateForm);
+    super.onClose();
+  }
 
   void addUser(String username) {
-    usernames.add(username);
+    if (username.isNotEmpty) {
+      usernames.add(username);
+    }
+  }
+
+  void toggleInputField() {
+    showInputField.value = !showInputField.value;
+  }
+
+  void addPlayer(String username) {
+    addUser(username);
+    toggleInputField();
+    usernameController.clear();
+  }
+
+  BorderSide focusedBorderSide() {
+    return BorderSide(color: AppColors.primaryColor);
+  }
+
+  BorderSide defaultBorderSide() {
+    return BorderSide(color: Colors.grey);
+  }
+
+  void validateForm() {
+    String? error = _usernameValidator(usernameController.text);
+    if (error != null) {
+      errorMessage.value = error;
+    } else {
+      errorMessage.value = '';
+    }
+    _updateCanProceed();
+  }
+
+  String? _usernameValidator(String value) {
+    if (_usernameValidationTimer != null) {
+      _usernameValidationTimer!.cancel();
+    }
+
+    _usernameValidationTimer = Timer(Duration(seconds: 1), () {
+      if (value.isEmpty) {
+        errorMessage.value = 'Username is required';
+      } else if (value.length < 4 || value.length > 15) {
+        errorMessage.value = 'Username must be between 4 and 15 characters';
+      } else if (value.contains(RegExp(r'\d')) && int.tryParse(value) != null) {
+        errorMessage.value = 'Username cannot contain only numbers';
+      } else if (value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+        errorMessage.value = 'Username cannot contain special characters';
+      } else if (usernames.contains(value)) {
+        errorMessage.value = "Username cannot match another players name!";
+      } else {
+        errorMessage.value = '';
+      }
+    });
+
+    return null;
+  }
+
+  void _updateCanProceed() {
+    canProceed.value = _canProceed();
+  }
+
+  bool _canProceed() {
+    final username = usernameController.text;
+    if (username.isEmpty ||
+        username.length < 4 ||
+        username.length > 15 ||
+        username.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]')) ||
+        usernames.contains(username)) {
+      return false;
+    }
+    return true;
   }
 }
--- lib/views/pages/03_local_play/local_play.dart
+++ lib/views/pages/03_local_play/local_play.dart
@@ -4,6 +4,7 @@ import '../../../constants/app_sizes.dart';
 import '../../themes/text.dart';
 import 'package:get/get.dart';
 import '../../../controllers/local_play_controller.dart';
+import '../../widgets/add_user_input_field.dart';
 
 class LocalPlayScreen extends StatelessWidget {
   final String username;
@@ -11,67 +12,131 @@ class LocalPlayScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final LocalPlayController localPlayController = Get.put(LocalPlayController());
+    final localPlayController = Get.put(LocalPlayController());
+    final TextEditingController _usernameController = TextEditingController();
+
     localPlayController.addUser(username);
-    
+
     return Scaffold(
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.all(AppSizes.defaultPadding),
           child: Column(
             children: [
-              Expanded(
-                child: Column(
-                  children: [
-                    SizedBox(height: 40),
+              SizedBox(height: 40),
 
-                    // Local Play Header Text
-                    Center(
-                      child: Text(
-                        "Local Play Lobby",
-                        style:
-                            TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-
-                    SizedBox(height: 40),
-
-                    // Those Drinking Header Text
-                    Center(
-                      child: Text(
-                        "Those Ready to Drink!",
-                        style:
-                            TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-
-                    // Container holding all usernames of people that are playing
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 20),
-                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 150),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200], // Light grey color
-                        borderRadius: BorderRadius.circular(10), // Rounded corners
-                      ),
-                      child: GetBuilder<LocalPlayController>(
-                        builder: (controller) {
-                          return Column(
-                            children: controller.usernames
-                                .map((user) => Text(user))
-                                .toList(),
-                          );
-                        },
-                      ),
-                    ),
-
-                    // Add player button
-
-                    // Input field shown when add player button tapped
-
-                    // Let's Play button
-                  ],
+              // Local Play Header Text
+              Center(
+                child: Text(
+                  "Local Play Lobby",
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
                 ),
               ),
+
+              SizedBox(height: 40),
+
+              // Those Drinking Header Text
+              Center(
+                child: Text(
+                  "Those Ready to Drink!",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              // Container holding all usernames of people that are playing
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 150),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200], // Light grey color
+                  borderRadius: BorderRadius.circular(10), // Rounded corners
+                ),
+                child: Obx(
+                  () => Column(
+                    children: localPlayController.usernames
+                        .map((user) => Text(user))
+                        .toList(),
+                  ),
+                ),
+              ),
+
+              Obx(
+                () => localPlayController.showInputField.value
+                    ? Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Obx(() {
+                            return localPlayController
+                                    .errorMessage.value.isEmpty
+                                ? SizedBox.shrink()
+                                : Text(
+                                    localPlayController.errorMessage.value,
+                                    style: TextStyle(color: Colors.red),
+                                  );
+                          }),
+                          AddUserField(
+                            onChanged: (_) =>
+                                localPlayController.validateForm(),
+                          ),
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment
+                                .end, // Align the icons to the right
+                            children: [
+                              // Cancel button
+                              IconButton(
+                                onPressed: () {
+                                  localPlayController.toggleInputField();
+                                  localPlayController.usernameController
+                                      .clear();
+                                },
+                                icon: Icon(Icons.cancel),
+                              ),
+                              SizedBox(width: 5),
+                              Obx(() {
+                                return IconButton(
+                                  onPressed:
+                                      localPlayController.canProceed.value
+                                          ? () => localPlayController.addPlayer(
+                                              localPlayController
+                                                  .usernameController.text)
+                                          : null,
+                                  icon: Icon(Icons.add),
+                                );
+                              }),
+                            ],
+                          ),
+                        ],
+                      )
+                    : ElevatedButton(
+                        onPressed: localPlayController.toggleInputField,
+                        child: Text('+ ADD PLAYER'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context)
+                              .primaryColor, // You can set the button color here
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                20), // Change the corner radius for rounded edges
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 30,
+                              vertical:
+                                  10), // Adjust the padding for a smaller size
+                        ),
+                      ),
+              ),
+
+              // Spacer to push the button to the bottom
+              Spacer(),
+
+              // LET'S PLAY button
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("LET'S PLAY!"),
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 50)),
+              ),
+              SizedBox(height: 10),
             ],
           ),
         ),
