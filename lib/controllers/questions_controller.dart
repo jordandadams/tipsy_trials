0492f4a37aa72ef23613babf02b6ed82313b621d
jordanadamstn <jordan.adams@tn.gov>
2023-03-18 16:56:27 -0500

    game screen with cards

---
+++ lib/controllers/questions_controller.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+class QuestionController extends GetxController {
+  late List<dynamic> _questions;
+  final Random _random = Random();
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadQuestions();
+  }
+
+  Future<void> loadQuestions() async {
+    String jsonString = await rootBundle.loadString('assets/data/questions.json');
+    final Map<String, dynamic> parsedData = jsonDecode(jsonString);
+    _questions = parsedData["vote_cards"] +
+        parsedData["truth_or_dare_cards"][0]["truth"] +
+        parsedData["truth_or_dare_cards"][0]["dare"];
+  }
+
+  Map<String, dynamic> getRandomQuestion() {
+    return _questions[_random.nextInt(_questions.length)];
+  }
+}
--- lib/views/pages/05_game/game.dart
+++ lib/views/pages/05_game/game.dart
@@ -1,22 +1,102 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import '../../../constants/app_images.dart';
-import '../../../constants/app_sizes.dart';
-import '../../themes/text.dart';
+import '../../../controllers/questions_controller.dart';
 import 'package:get/get.dart';
 
-class GameScreen extends StatelessWidget {
+class GameScreen extends StatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  bool _isFront = true;
+  final QuestionController _questionController = Get.put(QuestionController());
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+      ),
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.all(AppSizes.defaultPadding),
-          child: Column(
-            children: [
-              Text('Game Screen')
-            ],
+        child: GestureDetector(
+          onTap: () {
+            _controller.forward();
+            setState(() {
+              _isFront = !_isFront;
+            });
+          },
+          child: Center(
+            child: AnimatedBuilder(
+              animation: _controller,
+              builder: (BuildContext context, Widget? child) {
+                return Transform(
+                  alignment: FractionalOffset.center,
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.001),
+                  child: _controller.value < 0.5 ? _buildFront() : _buildBack(),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFront() {
+    return Container(
+      height: 300,
+      width: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.red,
+      ),
+      child: Center(child: Text('Tap to flip')),
+    );
+  }
+
+  Widget _buildBack() {
+    Map<String, dynamic> question = _questionController.getRandomQuestion();
+    return Container(
+      height: 300,
+      width: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.blue,
+      ),
+      child: Center(
+        child: Text(
+          question["question"] ?? '',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
           ),
         ),
       ),
